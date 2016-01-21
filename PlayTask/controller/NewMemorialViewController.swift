03c22950834af60d8e31be0ab6a827b6b64dd229
Yoncise <yoncise@gmail.com>
2016-01-21 16:45:42 +0800

    new commerial view

---
+++ PlayTask/controller/NewMemorialViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NewMemorialViewController.swift
+//  PlayTask
+//
+//  Created by Yoncise on 1/21/16.
+//  Copyright © 2016 yon. All rights reserved.
+//
+
+import UIKit
+
+class NewMemorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancel(sender: UIBarButtonItem) {
+        self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
